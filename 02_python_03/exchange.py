752a682010f32c5ba6b42a39b4474ee0cc73be84
Konstantin Sopov <sopov.ks@gmail.com>
2018-12-14 04:01:41 +0300

    homework 3

---
+++ 02_python_03/exchange.py
@@ -0,0 +1,70 @@
+# coding: utf-8
+import requests
+
+class Rate:
+    def __init__(self, format='value', diff=False):
+        self.format = format
+        self.diff = diff
+    
+    def exchange_rates(self):
+        """
+        Возвращает ответ сервиса с информацией о валютах в виде:
+        
+        {
+            'AMD': {
+                'CharCode': 'AMD',
+                'ID': 'R01060',
+                'Name': 'Армянских драмов',
+                'Nominal': 100,
+                'NumCode': '051',
+                'Previous': 14.103,
+                'Value': 14.0879
+                },
+            ...
+        }
+        """
+        r = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+        return r.json()['Valute']
+    
+    def make_format(self, currency):
+        """
+        Возвращает информацию о валюте currency в двух вариантах:
+        - полная информация о валюте при self.format = 'full':
+        Rate('full').make_format('EUR')
+        {
+            'CharCode': 'EUR',
+            'ID': 'R01239',
+            'Name': 'Евро',
+            'Nominal': 1,
+            'NumCode': '978',
+            'Previous': 79.6765,
+            'Value': 79.4966
+        }
+        
+        Rate('value').make_format('EUR')
+        79.4966
+        """
+        response = self.exchange_rates()
+        
+        if currency in response:
+            if self.format == 'full':
+                return response[currency]
+            
+            if self.format == 'value' and self.diff == False:
+                return response[currency]['Value']
+            
+            if self.format == 'value' and self.diff == True:
+                return (response[currency]['Value']-response[currency]['Previous'])
+            
+            if self.format == 'name':
+                return response[currency]['Name']
+        
+        return 'Error'
+    
+    def eur(self):
+        """Возвращает курс евро на сегодня в формате self.format"""
+        return self.make_format('EUR')
+    
+    def usd(self):
+        """Возвращает курс доллара на сегодня в формате self.format"""
+        return self.make_format('USD')
